b34c85cdff8e5b1d321cf92865adff10a62de0b6
yusra230 <yusra4501434@cloud.neduet.edu.pk>
2024-08-06 18:58:27 +0500

    first commit

--- src/components/EducatorPlus.jsx
+++ src/components/EducatorPlus.jsx
@@ -1,9 +1,157 @@
 const EducatorPlus = () => {
     return <>
-    <div className="bg-[url('bg-texture.png')] bg-[#2C3162] py-10">
+        {/* <div className="bg-[url('bg-texture.png')] bg-[#2C3162] py-10">
         <h2 className="text-3xl text-center font-bold text-white">Invest in your career with Educator Plus</h2>
-    </div>
+        <p className="text-center text-gray-300">Get access to videos in over 90% of courses, Specializations, and Professional Certificates taught by top instructors from leading universities and companies.</p>
+    </div> */}
+
+        <section className="bg-gray-900 text-white">
+            <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-20">
+                <div className="mx-auto max-w-2xl text-center">
+                    <h2 className="text-3xl font-bold sm:text-4xl">Invest in your career with Educator Plus</h2>
+
+                    <p className="mt-4 text-gray-300 py-4">
+                        Get access to videos in over 90% of courses, Specializations, and Professional Certificates taught by top instructors from leading universities and companies.
+                    </p>
+                </div>
+
+                <div className="mt-8 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4 text-center">
+                    <div
+                        className="block rounded-xl border border-gray-800 p-6 shadow-xl transition hover:border-indigo-500/10 hover:shadow-indigo-600/10"
+                        href="#"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="size-10 text-[#4F46E5] mx-auto"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                            <path
+                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+                            />
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                            />
+                        </svg>
+
+                        <h2 className="mt-4 text-xl font-bold text-white">Learn anything</h2>
+
+                        <p className="mt-1 text-sm text-gray-300">
+                            Explore any interest or trending topic, take prerequisites, and advance your skills
+                        </p>
+                    </div>
+
+                    <div
+                        className="block rounded-xl border border-gray-800 p-6 shadow-xl transition hover:border-indigo-500/10 hover:shadow-indigo-600/10"
+                        href="#"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="size-10 text-[#4F46E5] mx-auto"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                            <path
+                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+                            />
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                            />
+                        </svg>
+
+                        <h2 className="mt-4 text-xl font-bold text-white">Save money</h2>
+
+                        <p className="mt-1 text-sm text-gray-300">
+                            Spend less money on your learning if you plan to take multiple courses this year
+                        </p>
+                    </div>
+
+                    <div
+                        className="block rounded-xl border border-gray-800 p-6 shadow-xl transition hover:border-indigo-500/10 hover:shadow-indigo-600/10"
+                        href="#"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="size-10 text-[#4F46E5] mx-auto"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                            <path
+                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+                            />
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                            />
+                        </svg>
+
+                        <h2 className="mt-4 text-xl font-bold text-white">Flexible learning</h2>
+
+                        <p className="mt-1 text-sm text-gray-300">
+                            Learn at your own pace, move between multiple courses, or switch to a different course
+                        </p>
+                    </div>
+
+                    <div
+                        className="block rounded-xl border border-gray-800 p-6 shadow-xl transition hover:border-indigo-500/10 hover:shadow-indigo-600/10"
+                        href="#"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="size-10 text-[#4F46E5] mx-auto"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                            <path
+                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+                            />
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                            />
+                        </svg>
+
+                        <h2 className="mt-4 text-xl font-bold text-white">Unlimited certificates</h2>
+
+                        <p className="mt-1 text-sm text-gray-300">
+                            Earn a certificate for every learning program that you complete after your trial ends at no additional cost
+                        </p>
+                    </div>
+
+                  
+
+                 
+                </div>
+
+                <div className="mt-12 text-center">
+                    <a
+                        href="#"
+                        className="inline-block rounded bg-indigo-600  px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none"
+                    >
+                        Start 7-day Free Trial
+                    </a>
+                </div>
+            </div>
+        </section>
     </>
 }
 
-export default EducatorPlus ;
+export default EducatorPlus;
--- src/components/TrustedCompanies.jsx
+++ src/components/TrustedCompanies.jsx
@@ -1,7 +1,7 @@
 const TrustedCompanies = () => {
     return <>
         <div className="bg-[#4F46E5] py-10">
-            <h6 className="text-center text-black text-lg mb-5 font-bold">Trusted by over 15,000 companies and millions of learners around the world</h6>
+            <h6 className="text-center text-white text-lg mb-5 font-bold">Trusted by over 15,000 companies and millions of learners around the world</h6>
 
             <div className="flex items-center justify-center space-x-14">
                 <img src="t1.png" className="w-40 align-middle" alt="" />
