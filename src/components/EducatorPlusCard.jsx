973f33751d0f4d71e9848cb53beb4b5558a1c8a2
yusra230 <yusra4501434@cloud.neduet.edu.pk>
2024-08-06 21:30:26 +0500

    first commit

---
+++ src/components/EducatorPlusCard.jsx
@@ -0,0 +1,24 @@
+const EducatorPlusCard = ({item}) => {
+    console.log('inside item');
+    console.log(item)
+    return <>
+        <a
+            className="block rounded-xl border border-gray-800 p-6 shadow-xl transition hover:border-indigo-500/10 hover:shadow-indigo-600/10"
+            href="#"
+        >
+            <img src={item.mainImg} alt="" />
+
+            <div className="mt-4 flex">
+                <img src={item.logo} className="w-6" alt="" />
+                <p className="ml-2">{item.logoName}</p>
+            </div>
+            <h2 className="mt-4 text-basic font-bold text-white">{item.title}</h2>
+
+            <p className="mt-4 text-sm text-gray-300">
+                Beginner - Course
+            </p>
+        </a>
+    </>
+}
+
+export default EducatorPlusCard;
--- src/components/EducatorPlusCardSection.jsx
+++ src/components/EducatorPlusCardSection.jsx
@@ -1,70 +1,55 @@
-const EducatorPlusCardSection=() => {
+import EducatorPlusCard from "./EducatorPlusCard"
+
+const EducatorPlusCardSection = () => {
+    let EducatorPlusObj = [
+        {
+            id: 1,
+            mainImg:'course1.jpg',
+            logo: 'googleL.png',
+            logoName: 'Google',
+            title: 'Google AI Essentials',
+
+        },
+
+        {
+            id: 2,
+            mainImg:'course2.jpg',
+            logo: 'logo2.png',
+            logoName: 'University of Michigan',
+            title: 'Python For Everybody',
+
+        },
+
+        {
+            id: 3,
+            mainImg:'course3.jpg',
+            logo: 'logo3.png',
+            logoName: 'Vanderbuilt University',
+            title: 'Prompt Engineering',
+
+        },
+
+        {
+            id: 4,
+            mainImg:'course4.jpg',
+            logo: 'logo4.png',
+            logoName: 'IBM',
+            title: 'IBM Data Science',
+
+        },
+    ]
     return <>
-     <section className="bg-gray-900 text-white">
+        <section className="bg-gray-900 text-white">
             <div className="mx-auto max-w-screen-xl px-4 sm:px-6 sm:py-12 lg:px-8">
                 <div className="mx-auto max-w-3xl text-center">
                     <h2 className="text-3xl font-bold sm:text-4xl">See what you can learn with Educator Plus</h2>
                 </div>
 
                 <div className="mt-8 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
-                    <a
-                        className="block rounded-xl border border-gray-800 p-6 shadow-xl transition hover:border-indigo-500/10 hover:shadow-indigo-600/10"
-                        href="#"
-                    >
-                       <img src="course1.jpg" alt="" />
-
-                       <div className="mt-2 flex">
-                        <img src="googleL.png" className="w-6" alt="" />
-                        <p className="ml-2">Google</p>
-                       </div>
-                        <h2 className="mt-4 text-basic font-bold text-white">Google AI Essentials</h2>
-
-                        <p className="mt-4 text-sm text-gray-300">
-                           Beginner - Course
-                        </p>
-                    </a>
-
-                    <div
-                        className="block rounded-xl border border-gray-800 p-6 shadow-xl transition hover:border-indigo-500/10 hover:shadow-indigo-600/10"
-                        href="#"
-                    >
-                       <img src="course2.jpg" alt="" />
-
-                        <h2 className="mt-4 text-xl font-bold text-white">Save money</h2>
-
-                        <p className="mt-1 text-sm text-gray-300">
-                            Spend less money on your learning if you plan to take multiple courses this year
-                        </p>
-                    </div>
-
-                    <div
-                        className="block rounded-xl border border-gray-800 p-6 shadow-xl transition hover:border-indigo-500/10 hover:shadow-indigo-600/10"
-                        href="#"
-                    >
-                        <img src="course3.jpg" alt="" />
-
-                        <h2 className="mt-4 text-xl font-bold text-white">Flexible learning</h2>
-
-                        <p className="mt-1 text-sm text-gray-300">
-                            Learn at your own pace, move between multiple courses, or switch to a different course
-                        </p>
-                    </div>
-
-                    <div
-                        className="block rounded-xl border border-gray-800 p-6 shadow-xl transition hover:border-indigo-500/10 hover:shadow-indigo-600/10"
-                        href="#"
-                    >
-                       <img src="course4.jpg" alt="" />
-                        <h2 className="mt-4 text-xl font-bold text-white">Unlimited certificates</h2>
-
-                        <p className="mt-1 text-sm text-gray-300">
-                            Earn a certificate for every learning program that you complete after your trial ends at no additional cost
-                        </p>
-                    </div>
-
-                  
-
-                 
+                    {EducatorPlusObj.map(item=>{
+                         return <EducatorPlusCard key={item.id} item={item}></EducatorPlusCard>
+                    })}
+                   
                 </div>
 
                 <div className="mt-4">
