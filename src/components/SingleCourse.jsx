19a2d20ffb7c0ef3737e299bb8bd2b41360e3a3d
yusra230 <yusra4501434@cloud.neduet.edu.pk>
2024-08-09 22:33:14 +0500

    first commit

--- src/components/SingleCourse.jsx
+++ src/components/SingleCourse.jsx
@@ -1,12 +1,48 @@
 import { useParams } from "react-router-dom";
+import courses from "../data/courses";
 
 const SingleCourse = () => {
     const params = useParams();
 
     const id = Number(params.id);
 
+    const course = courses.find(course => course.id === id);
+    console.log(course);
+
     return <>
-        <h1>{id}</h1>
+        <section className="bg-gray-900 text-white">
+            <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+                <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16 items-center">
+                    <div className="relative h-50 overflow-hidden rounded-lg sm:h-80 lg:order-last">
+                        <img
+                            alt=""
+                            src={course.mainImg}
+                            className="absolute inset-0 h-full w-full object-cover"
+                        />
+                    </div>
+
+                    <div className="lg:py-24">
+                        <div className="flex items-center mb-4">
+                            <img src={course.logo} className="w-10 mr-4" alt="" />
+                            <p>{course.logoName}</p>
+                        </div>
+                        <h2 className="text-3xl font-bold sm:text-4xl">{course.title}</h2>
+
+                        <p className="mt-4 text-gray-300">
+                            {course.description}
+                        </p>
+
+                        <a
+                            href="#"
+                            className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none"
+                        >
+                           Enroll Today
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </>
 }
 
--- src/data/courses.js
+++ src/data/courses.js
@@ -1,154 +1,170 @@
 let courses = [
     {
         id: 1,
-        mainImg: 'course1.jpg',
-        logo: 'googleL.png',
+        mainImg: '/course1.jpg',
+        logo: '/googleL.png',
         logoName: 'Google',
         title: 'Google AI Essentials',
-
+        description: 'Launch your career in Google AI Essentials. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
     },
 
     {
         id: 2,
-        mainImg: 'course2.jpg',
-        logo: 'logo2.png',
+        mainImg: '/course2.jpg',
+        logo: '/logo2.png',
         logoName: 'University of Michigan',
         title: 'Python For Everybody',
+        description: 'Launch your career in Python. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 3,
-        mainImg: 'course3.jpg',
-        logo: 'logo3.png',
+        mainImg: '/course3.jpg',
+        logo: '/logo3.png',
         logoName: 'Vanderbuilt University',
         title: 'Prompt Engineering',
+        description: 'Launch your career in Prompt Engineering. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 4,
-        mainImg: 'course4.jpg',
-        logo: 'logo4.png',
+        mainImg: '/course4.jpg',
+        logo: '/logo4.png',
         logoName: 'IBM',
-        title: 'IBM Data Science',
+        title: 'Data Science',
+        description: 'Launch your career in Data Science. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 5,
-        mainImg: 'course5.jpg',
-        logo: 'googleL.png',
+        mainImg: '/course5.jpg',
+        logo: '/googleL.png',
         logoName: 'Google',
         title: 'Data Science',
+        description: 'Launch your career in Data Science. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 6,
-        mainImg: 'course6.jpg',
-        logo: 'logo4.png',
+        mainImg: '/course6.jpg',
+        logo: '/logo4.png',
         logoName: 'IBM',
         title: 'Artificial Intelligance',
+        description: 'Launch your career in Artificial Intelligance. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 7,
-        mainImg: 'course7.jpg',
-        logo: 'logo2.png',
+        mainImg: '/course7.jpg',
+        logo: '/logo2.png',
         logoName: 'University of Michigan',
         title: 'Web Development',
+        description: 'Launch your career in Web Development. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 8,
-        mainImg: 'course8.jpg',
-        logo: 'logo3.png',
+        mainImg: '/course8.jpg',
+        logo: '/logo3.png',
         logoName: 'Vanderbuilt University',
         title: 'App Development',
+        description: 'Launch your career in App Development. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 9,
-        mainImg: 'course9.jpg',
-        logo: 'googleL.png',
+        mainImg: '/course9.jpg',
+        logo: '/googleL.png',
         logoName: 'Google',
         title: 'Frontend Development',
+        description: 'Launch your career in Frontend Development. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 10,
-        mainImg: 'course10.jpg',
-        logo: 'logo2.png',
+        mainImg: '/course10.jpg',
+        logo: '/logo2.png',
         logoName: 'University of Michigan',
         title: 'Machine Learning',
+        description: 'Launch your career in Machine Learning. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 11,
-        mainImg: 'course11.jpg',
-        logo: 'googleL.png',
+        mainImg: '/course11.jpg',
+        logo: '/googleL.png',
         logoName: 'Google',
         title: 'Deep Learning',
+        description: 'Launch your career in Deep Learning. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 12,
-        mainImg: 'course12.jpg',
-        logo: 'logo4.png',
+        mainImg: '/course12.jpg',
+        logo: '/logo4.png',
         logoName: 'IBM',
         title: 'Cloud Computing',
+        description: 'Launch your career in Cloud Computing. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 13,
-        mainImg: 'course13.jpg',
-        logo: 'googleL.png',
+        mainImg: '/course13.jpg',
+        logo: '/googleL.png',
         logoName: 'Google',
         title: 'AJAX development',
+        description: 'Launch your career in AJAX development. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 14,
-        mainImg: 'course14.jpg',
-        logo: 'logo2.png',
+        mainImg: '/course14.jpg',
+        logo: '/logo2.png',
         logoName: 'University of Michigan',
         title: 'API Integration',
+        description: 'Launch your career in API Integration. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 15,
-        mainImg: 'course15.jpg',
-        logo: 'logo4.png',
+        mainImg: '/course15.jpg',
+        logo: '/logo4.png',
         logoName: 'IBM',
         title: 'Rust',
+        description: 'Launch your career in Rust. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 16,
-        mainImg: 'course16.jpg',
-        logo: 'googleL.png',
+        mainImg: '/course16.jpg',
+        logo: '/googleL.png',
         logoName: 'Google',
         title: 'Java',
+        description: 'Launch your career in Java. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 
     {
         id: 17,
-        mainImg: 'course17.jpg',
-        logo: 'logo3.png',
+        mainImg: '/course17.jpg',
+        logo: '/logo3.png',
         logoName: 'Vanderbuilt University',
         title: 'React',
+        description: 'Launch your career in React. Gain the in-demand skills and hands-on experience to get job-ready in less than 4 months. No prior experience required.'
 
     },
 ];
